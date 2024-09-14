42c0e20b136e418503e2fb50944e1de250178cfb
Dwyor-tmx <86127970+Dwyor-tmx@users.noreply.github.com>
2024-09-14 18:16:11 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -104,7 +104,7 @@ FILES=Image.gz
 
 # Build dtbo.img (select this only if your source has support to building dtbo.img)
 # 1 is YES | 0 is NO(default)
-BUILD_DTBO=1
+BUILD_DTBO=0
 pacman -Syu --noconfirm python-pip
 
 # Sign the zipfile
@@ -409,7 +409,7 @@ gen_zip()
 {
 	msger -n "|| Zipping into a flashable zip ||"
 	mv "$KERNEL_DIR"/out/arch/arm64/boot/$FILES AnyKernel3/$FILES
-	if [ $BUILD_DTBO = 1 ]
+	if [ $BUILD_DTBO = 0 ]
 	then
 		mv "$KERNEL_DIR"/out/arch/arm64/boot/dtbo.img AnyKernel3/dtbo.img
 	fi
