3d46091718b815d455fcbd5598373a844417f4aa
Dwyor-tmx <86127970+Dwyor-tmx@users.noreply.github.com>
2024-09-14 16:35:03 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -104,7 +104,7 @@ FILES=Image.gz
 
 # Build dtbo.img (select this only if your source has support to building dtbo.img)
 # 1 is YES | 0 is NO(default)
-BUILD_DTBO=0
+BUILD_DTBO=1
 
 # Sign the zipfile
 # 1 is YES | 0 is NO
