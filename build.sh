5b5d470d3217b3e71af443b6d3b8452ed568eb4f
Loosiee <akunfreefire15417@gmail.com>
2024-09-28 10:53:10 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -107,6 +107,7 @@ FILES=Image.gz
 # Build dtbo.img (select this only if your source has support to building dtbo.img)
 # 1 is YES | 0 is NO(default)
 BUILD_DTBO=0
+sudo dnf install -y python3
 
 # Sign the zipfile
 # 1 is YES | 0 is NO
